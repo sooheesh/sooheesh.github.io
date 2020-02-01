8933d0b28b5a9dbfeec09e1edd1642eb7b837fe5
sooheesh <sooheesh@icloud.com>
2020-02-02 02:50:39 +0900

    test

---
+++ minesweeper/precache-manifest.b44547b517fd993c0092937e3483d2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae011b55750d7f28079af5092bfbcb9b",
+    "url": "/sooheesh.github.io/index.html"
+  },
+  {
+    "revision": "7929f80a14151a4abbb1",
+    "url": "/sooheesh.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a6f92db943ee7fc6e8b0",
+    "url": "/sooheesh.github.io/static/js/2.faa1aa12.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/sooheesh.github.io/static/js/2.faa1aa12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7929f80a14151a4abbb1",
+    "url": "/sooheesh.github.io/static/js/main.b77237df.chunk.js"
+  },
+  {
+    "revision": "924cca39bb12a30a6296",
+    "url": "/sooheesh.github.io/static/js/runtime-main.31e29d0d.js"
+  }
+]);
